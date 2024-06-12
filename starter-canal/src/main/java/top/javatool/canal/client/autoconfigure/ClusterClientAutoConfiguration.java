8c0c1d68183c3eba72d0914d08f202c3749fe614
wubixuan <wubixuan@qq.com>
2024-06-12 17:33:03 +0800

    Initial commit

---
+++ starter-canal/src/main/java/top/javatool/canal/client/autoconfigure/ClusterClientAutoConfiguration.java
@@ -0,0 +1,73 @@
+package top.javatool.canal.client.autoconfigure;
+
+
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import top.javatool.canal.client.client.ClusterCanalClient;
+import top.javatool.canal.client.factory.EntryColumnModelFactory;
+import top.javatool.canal.client.handler.EntryHandler;
+import top.javatool.canal.client.handler.MessageHandler;
+import top.javatool.canal.client.handler.RowDataHandler;
+import top.javatool.canal.client.handler.impl.AsyncMessageHandlerImpl;
+import top.javatool.canal.client.handler.impl.RowDataHandlerImpl;
+import top.javatool.canal.client.handler.impl.SyncMessageHandlerImpl;
+import top.javatool.canal.client.properties.CanalProperties;
+import top.javatool.canal.client.properties.CanalSimpleProperties;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+@Configuration
+@EnableConfigurationProperties(CanalSimpleProperties.class)
+@ConditionalOnBean(value = {EntryHandler.class})
+@ConditionalOnProperty(value = CanalProperties.CANAL_MODE, havingValue = "cluster")
+@Import(ThreadPoolAutoConfiguration.class)
+public class ClusterClientAutoConfiguration {
+
+
+    private CanalSimpleProperties canalSimpleProperties;
+
+
+    public ClusterClientAutoConfiguration(CanalSimpleProperties canalSimpleProperties) {
+        this.canalSimpleProperties = canalSimpleProperties;
+    }
+
+    @Bean
+    public RowDataHandler<CanalEntry.RowData> rowDataHandler() {
+        return new RowDataHandlerImpl(new EntryColumnModelFactory());
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = CanalProperties.CANAL_ASYNC, havingValue = "true", matchIfMissing = true)
+    public MessageHandler messageHandler(RowDataHandler<CanalEntry.RowData> rowDataHandler, List<EntryHandler> entryHandlers,
+                                         ExecutorService executorService) {
+        return new AsyncMessageHandlerImpl(entryHandlers, rowDataHandler, executorService);
+    }
+
+
+    @Bean
+    @ConditionalOnProperty(value = CanalProperties.CANAL_ASYNC, havingValue = "false")
+    public MessageHandler messageHandler(RowDataHandler<CanalEntry.RowData> rowDataHandler, List<EntryHandler> entryHandlers) {
+        return new SyncMessageHandlerImpl(entryHandlers, rowDataHandler);
+    }
+
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public ClusterCanalClient clusterCanalClient(MessageHandler messageHandler) {
+        return ClusterCanalClient.Builder.builder().
+                canalServers(canalSimpleProperties.getServer())
+                .destination(canalSimpleProperties.getDestination())
+                .userName(canalSimpleProperties.getUserName())
+                .messageHandler(messageHandler)
+                .password(canalSimpleProperties.getPassword())
+                .batchSize(canalSimpleProperties.getBatchSize())
+                .filter(canalSimpleProperties.getFilter())
+                .timeout(canalSimpleProperties.getTimeout())
+                .unit(canalSimpleProperties.getUnit()).build();
+    }
+}
