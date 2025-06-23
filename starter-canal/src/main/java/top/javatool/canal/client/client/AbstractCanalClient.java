b4996dbecf0c3a7433ef92c6602d045c3662d329
wubixuan <wubixuan@qq.com>
2025-06-23 16:45:44 +0800

    提交一个测试4

--- starter-canal/src/main/java/top/javatool/canal/client/client/AbstractCanalClient.java
+++ starter-canal/src/main/java/top/javatool/canal/client/client/AbstractCanalClient.java
@@ -81,6 +81,7 @@ public abstract class AbstractCanalClient implements CanalClient {
                     log.info("获取消息 {}", message);
                     log.info("获取消息 {}", message);
                     log.info("获取消息 {}", message);
+                    log.info("获取消息 {}", message);
                     connector.ack(batchId);
                 }
             } catch (Exception e) {
